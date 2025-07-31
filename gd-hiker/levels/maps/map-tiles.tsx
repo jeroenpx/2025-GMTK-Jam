abd5a5a1b06c6b5590d98f8788e3aea16054d9f2
Jeroen Penninck <jeroen.penninck@gmail.com>
2025-07-31 14:48:30 +0200

    Adding test hexagon tile map to read

---
+++ gd-hiker/levels/maps/map-tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="map-tiles" tilewidth="32" tileheight="32" tilecount="13" columns="13">
+ <image source="map-tiles.png" width="445" height="32"/>
+</tileset>
