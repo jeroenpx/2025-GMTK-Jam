ee17dec7db38bbab69b5cd8f6a10f3febd7b1167
Jeroen Penninck <jeroen.penninck@gmail.com>
2025-07-31 19:14:29 +0200

    Dropping existing button indicators system in project

---
+++ gd-hiker/systems/button_indications/btn_indicators.gd
@@ -0,0 +1,37 @@
+extends Node
+
+@export var navigate_indication: Control;
+@export var undo_indication: Control;
+
+var showing_navigate = false;
+var showing_undo = false;
+
+func _ready() -> void:
+	_update_visibilities()
+	
+	# TODO: enable again
+	#GameState.on_game_state_changed.connect(_on_state_change);
+
+#func _on_state_change() -> void:
+#	if GameState.isGameplayRunning():
+#		_update_visibilities();
+#	else:
+#		navigate_indication.visible = false;
+#		undo_indication.visible = false;
+
+func show_navigate(show: bool) -> void:
+	showing_navigate = show;
+	_update_visibilities();
+
+func show_undo(show: bool) -> void:
+	showing_undo = show;
+	_update_visibilities();
+
+func _update_visibilities() -> void:
+	navigate_indication.visible = false;
+	undo_indication.visible = false;
+	
+	if showing_undo:
+		undo_indication.visible = true;
+	elif showing_navigate:
+		navigate_indication.visible = true;
