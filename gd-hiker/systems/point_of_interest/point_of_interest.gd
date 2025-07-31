ddaf97c925f4ad180b83a32bd62ac0260ad4c146
eevrip <elen.evri9@gmail.com>
2025-07-31 17:48:52 +0200

    Loop Manager and Point of interest
    Main logic of loop manager and pts of interest class

---
+++ gd-hiker/systems/point_of_interest/point_of_interest.gd
@@ -0,0 +1,31 @@
+class_name PointOfInterest
+extends Node3D
+@export var identifier: int
+@export var neighbours: Array[int] #possible neighbours
+var reference_point: Vector3 #reference point that hexagon map will read
+var is_visited: bool
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func on_clicked(current_visit: PointOfInterest) -> PointOfInterest:
+	print("click " + str(identifier))
+	is_visited = true
+	print("move to " + str(identifier))
+	return self
+
+
+
+func can_visit(pointId: int) -> bool:
+	if is_visited:
+		return false
+	for neighbour in neighbours:
+		if pointId == neighbour:
+			return true 
+	return false
